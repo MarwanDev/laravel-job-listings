c5c0e63bf34e9b0a9216b65833a3c43b52cd5495
Marwan <marwaneg@aucegypt.edu>
2023-11-20 13:59:11 +0200

    Create single listing view

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,7 +5,43 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Listing extends Model
+class Listing
 {
-    use HasFactory;
+    public static function all()
+    {
+
+        // use HasFactory;
+        return [
+            [
+                'id' => 1,
+                'title' => 'Laravel Senior Developer',
+                'tags' => 'laravel, javascript',
+                'company' => 'Acme Corp',
+                'location' => 'Boston, MA',
+                'email' => 'email1@email.com',
+                'website' => 'https://www.acme.com',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Laravel Senior Developer',
+                'tags' => 'laravel, javascript',
+                'company' => 'Acme Corp',
+                'location' => 'Boston, MA',
+                'email' => 'email1@email.com',
+                'website' => 'https://www.acme.com',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+            ],
+        ];
+    }
+
+    public static function find($id) {
+        $listings = self::all();
+
+        foreach($listings as $listing) {
+            if($listing['id'] == $id) {
+                return $listing;
+            }
+        }
+    }
 }
