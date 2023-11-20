0944a723405fa9058e900d7f61473d16d5e55105
Marwan <marwaneg@aucegypt.edu>
2023-11-20 12:43:26 +0200

    Create listing model

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('listings');
+    return view('listings', [
+        'heading' => 'Latest Listings'
+    ]);
 });
