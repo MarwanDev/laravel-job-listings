da662d4b7d3a9c6dedae3ed5d3b14443d118fdad
Marwan <marwaneg@aucegypt.edu>
2023-11-21 02:41:48 +0200

    Create listing model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,43 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Listing
+class Listing extends Model
 {
-    public static function all()
-    {
-
-        // use HasFactory;
-        return [
-            [
-                'id' => 1,
-                'title' => 'Laravel Senior Developer',
-                'tags' => 'laravel, javascript',
-                'company' => 'Acme Corp',
-                'location' => 'Boston, MA',
-                'email' => 'email1@email.com',
-                'website' => 'https://www.acme.com',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Laravel Senior Developer',
-                'tags' => 'laravel, javascript',
-                'company' => 'Acme Corp',
-                'location' => 'Boston, MA',
-                'email' => 'email1@email.com',
-                'website' => 'https://www.acme.com',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
-            ],
-        ];
-    }
-
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
