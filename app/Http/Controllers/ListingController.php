fa88846eb0964aa31c46c64c3b68839b77d7d14e
Marwan <marwaneg@aucegypt.edu>
2023-11-23 22:35:18 +0200

    Change index router from listing all to latest

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -8,7 +8,7 @@ class ListingController extends Controller
 {
     public function index() {
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->get()
         ]);
     }
 
