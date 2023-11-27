5ee01e26495a153623f601edde6d9ede8b1ef71f
Marwan <marwaneg@aucegypt.edu>
2023-11-27 14:20:47 +0200

    Add pagination

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -2,13 +2,19 @@
     @include('partials._hero')
     @include('partials._search')
 
-    @unless(count($listings) == 0)
+        @unless(count($listings) == 0)
 
-    @foreach ($listings as $listing)
-        <x-listing-card :listing="$listing" />
-    @endforeach
+        @foreach ($listings as $listing)
+            <x-listing-card :listing="$listing" />
+        @endforeach
 
-    @else
-    <p>No listings found</p>
-    @endunless
+        @else
+        <p>No listings found</p>
+        @endunless
+        
+
+
+    <div class="mt-6 p-4">
+        {{$listings->links()}}
+    </div>
 </x-layout>
