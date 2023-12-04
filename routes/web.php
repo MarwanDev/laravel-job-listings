24ffd1ad0a011ae973cd0e8592e33f4396be7707
Marwan <marwaneg@aucegypt.edu>
2023-12-04 16:00:01 +0200

    Create register user view

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListingController;
-use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,3 +40,6 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Show Register/Create Form
 Route::get('/register', [UserController::class, 'create']);
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
